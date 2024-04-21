7ff2379da240ec11677b77b778cd50d054acd6c5
Ilya Novikov <illianovikovnitzana@gmail.com>
2024-04-21 19:51:11 +0700

    update classes.py

--- src/classes.py
+++ src/classes.py
@@ -6,6 +6,9 @@ class Payment:
         self.name = name
         self.number = number
 
+    def __repr__(self):  # pragma: nocover
+        return f'Payment(name={self.name}, number={self.number})'
+
     @classmethod
     def init_from_payment(cls, payment):
         *name, number = payment.split(' ')
@@ -14,11 +17,25 @@ class Payment:
             number
         )
 
-    def __repr__(self):
-        return f'Payment(name={self.name}, number={self.number})'
-
     def safe(self):
-        return f'{self.name} {self.number}'
+        if self.name.lower() == 'счет':
+            safe_number = self.get_safe_account()
+        else:
+            safe_number = self.get_safe_card_number()
+            safe_number = self.split_card_number_by_blocks(safe_number)
+        return f'{self.name} {safe_number}'
+
+    def get_safe_account(self):
+        return '*' * 2 + self.number[-4:]
+
+    def get_safe_card_number(self):
+        start, middle, end = self.number[:6], self.number[6:-4], self.number[-4:]
+        safe_card_number = start + "*" * len(middle) + end
+        return safe_card_number
+
+    @staticmethod
+    def split_card_number_by_blocks(card_number):
+        return f'{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}'
 
 
 class Amount:
@@ -27,7 +44,7 @@ class Amount:
         self.currency_name = currency_name
         self.currency_code = currency_code
 
-    def __repr__(self):
+    def __repr__(self):  # pragma: nocover
         return f'Amount(value={self.value}, currency_name={self.currency_name})'
 
 
@@ -50,6 +67,13 @@ class Operation:
         self.op_to = op_to
         self.op_from = op_from
 
+    def __repr__(self):  # pragma: nocover
+        return (
+            f'Operation('
+            f'id={self.op_id}, description={self.op_description}, state={self.op_state}, date={self.op_date}, '
+            f'amount={self.op_amount}, from={self.op_from}, to={self.op_to}'
+        )
+
     @classmethod
     def init_from_dict(cls, data):
         return cls(
@@ -66,9 +90,13 @@ class Operation:
             op_from=Payment.init_from_payment(data['from']) if 'from' in data else None
         )
 
-    def __repr__(self):
-        return (
-            f'Operation('
-            f'id={self.op_id}, description={self.op_description}, state={self.op_state}, date={self.op_date}, '
-            f'amount={self.op_amount}, from={self.op_from}, to={self.op_to}'
-        )
+    def safe(self):
+        lines = [
+         f'{self.op_date.strftime("%d.%m.%Y")} {self.op_description}'
+        ]
+        if self.op_from:
+            lines.append(f'{self.op_from.safe()} -> {self.op_to.safe()}')
+        else:
+            lines.append(f'{self.op_to.safe()}')
+        lines.append(f'{self.op_amount.value:.2f} {self.op_amount.currency_name}')
+        return '\n'.join(lines)
